ac918f628ee3fd116aa3e3745c597ce79305b372
Aerendir <hello@aerendir.me>
2016-09-26 17:00:01 +0200

    Feature: Define the lenght of the columns.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,7 @@
+## 0.2.0
+*(2016-09-26)*
 
+#### Features
+* Define the lenght of the columns.
+
+---
--- example/example.php
+++ example/example.php
@@ -10,7 +10,7 @@ $data = [
         'review'=> 'Una misteriosa creatura marina che affonda navi da guerra di ogni paese, la scoperta di uno straordinario mezzo meccanico - il sommergibile Nautilus capace di esplorare le profondità degli abissi marini, il suo comandante, il Capitano Nemo, un uomo nobile e generoso che cela però nell\'animo un insopprimibile desiderio di odio e di vendetta, il professor Arronax, il suo servitore Consiglio e il fiociniere Ned Land. E poi le meraviglie di un continente sconosciuto - l\'oceano e i suoi abissi - le esplorazioni attraverso tutti i mari, le lotte contro mostri spaventosi, la fede in una scienza che dovrebbe offrire all\'uomo un progresso inarrestabile, il desiderio di libertà che anima tutti i personaggi del libro.'
     ],
     [
-        'title'  => 'Il signore degli anelli: La compagnia dell\'anello',
+        'title'  => 'Il signore degli anelli:        La compagnia dell\'anello',
         'author' => 'J. R. R. Tolkien',
         'review' => 'In questo primo romanzo della trilogia di Tolkien, il lettore conosce gli Hobbit, minuscoli esseri saggi e longevi. Frodo, venuto in possesso dell\'Anello del Potere, è costretto a partire per il paese delle tenebre. Un gruppo di Hobbit lo accompagna e, strada facendo, si associano alla compagnia altri esseri: Elfi, Nani e Uomini, anch\'essi legati al destino di Frodo. Le tappe del cammino li conducono attraverso molte esperienze diverse, finché la scomparsa di Gandalf, trascinato negli abissi da un\'orrenda creatura, li lascia senza guida. Così si scioglie la Compagnia dell\'Anello e i suoi membri si disperdono, minacciati da forze tenebrose, mentre la meta sembra disperatamente allontanarsi.'
     ],
@@ -24,18 +24,19 @@ $data = [
 $table = new \SerendipityHQ\Component\PHPTextMatrix\PHPTextMatrix($data);
 
 echo '<pre>';
-echo $table->render();
+//echo $table->render();
 echo '</pre>';
 
 // The array containing the data
 $options = [
     'columns' => [
         'review' => [
-            'max_width' => 300
+            'max_width' => 50
         ],
         'title' => [
-            'max_width' => 40,
-            'word_break' => 'break_all'
+            'max_width' => 20,
+            // Equal to CSS word-break: break-all
+            'cut' => true
         ]
     ]
 ];
