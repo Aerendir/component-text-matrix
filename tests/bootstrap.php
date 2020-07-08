e64f9e30a9024b89e7c2e3d69f8dde67b8886a5c
Aerendir <hello@aerendir.me>
2020-07-08 18:46:34 +0200

    Initial configuration of tools.

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Digital Certificate Verifier Component.
+ *
+ * Copyright Adamo Aerendir Crespi 2016-2017.
+ *
+ * @author    Adamo Aerendir Crespi <hello@aerendir.me>
+ * @copyright Copyright (C) 2016 - 2020 Aerendir. All rights reserved.
+ * @license   PRIVATE. No distribution, no copy, no derivative, no divulgation or any other activity or action that
+ *            could disclose this text.
+ */
+
+// This file is not required locally, but required by GitHub Actions
+require_once __DIR__ . '/../vendor/autoload.php';
